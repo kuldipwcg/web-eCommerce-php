19a4799e5dc3ebb4d12fb66c45dbbeeac97778f9
Krishna <krishnavachhani09@gmail.com>
2024-06-27 20:04:09 +0530

    All branch merged in Developer

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,9 +19,7 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
-        'first_name',
-        'last_name',
+        
         'email',
         'first_name',
         'last_name',
