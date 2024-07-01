0c6e376f997b8a1b0d25d9c314db02af29adec38
Krishna <krishnavachhani09@gmail.com>
2024-07-01 10:05:41 +0530

    mearge om_admin to developer

--- app/Http/Controllers/SubCategoryController.php
+++ app/Http/Controllers/SubCategoryController.php
@@ -4,7 +4,11 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\SubCategoryRequest;
 use App\Models\SubCategory;
+use App\Http\Requests\SubCategoryRequest;
+use App\Models\SubCategory;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
 use Illuminate\Support\Facades\DB;
 
 
@@ -13,21 +17,14 @@ class SubCategoryController extends Controller
     
         public function index()
         {
-            $sub_category=subCategory::with('Category')->latest()->paginate(10);
-            //dd($sub_category);
-            return response()->json([
-                'type'=>'success',
-                'message'=>'Category showed successfully',
-                'code'=>200,
-                'data'=>$sub_category
-            ]);
+            return response()->json(Subcategory::all());
         }
 
     public function store(SubCategoryRequest $request){
         
         $sub_category = Subcategory::create($request->all());
         
-        return response()->json($sub_category, 201);
+            return response()->json($sub_category, 201);
     }
     public function show($id)    
     {
@@ -57,4 +54,6 @@ class SubCategoryController extends Controller
         $sub_category->delete();
         return response()->json(['message' => 'Sub_Category deleted successfully']);
     }
+
+   
 }
