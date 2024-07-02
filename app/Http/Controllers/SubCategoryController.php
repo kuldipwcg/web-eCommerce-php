bf95bc40450d351d29115d22a206c17aafb3dd85
Krishna <krishnavachhani09@gmail.com>
2024-07-02 15:19:00 +0530

    category,sub-category and validation from admin side

--- app/Http/Controllers/SubCategoryController.php
+++ app/Http/Controllers/SubCategoryController.php
@@ -17,12 +17,6 @@ class SubCategoryController extends Controller
     
         public function index()
         {
-<<<<<<< HEAD
-<<<<<<< HEAD
-            return response()->json(Subcategory::latest()->paginate(10));
-=======
-=======
->>>>>>> 7363e18 (category,sub-category and cart api)
             $sub_category=subCategory::with('Category')->latest()->paginate(10);
             //dd($sub_category);
             return response()->json($sub_category);
