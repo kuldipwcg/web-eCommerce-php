7bc9513010a59b1e4b20bacd7130be046a9e807d
Om2734 <12102040703022@mbit.edu.in>
2024-07-11 13:17:47 +0530

    import validator class

--- app/Http/Requests/ReviewRequest.php
+++ app/Http/Requests/ReviewRequest.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Http\Exceptions\HttpResponseException;
+
 class ReviewRequest extends FormRequest
 {
     /**
